4edd5ee2a3ab25a4141def1136cf77a5317fa870
khanghoang <hoangtrieukhang@gmail.com>
2014-11-17 22:46:11 +0700

    Add micropost model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  resources :microposts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # root 'application#goodbye'
+  resources :users
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +18,6 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   # resources :products
-  resources :users
 
   # Example resource route with options:
   #   resources :products do
